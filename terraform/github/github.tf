38c172226d76fafb970ba7b6a72d1bc975aecc86
julimax <julimax951@gmail.com>
2023-04-07 23:31:13 -0300

    provisioners

--- terraform/github/github.tf
+++ terraform/github/github.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "github" {
-  token = "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"
+  token = "github_pat_token"
 }
 
 resource "github_repository" "example" {
